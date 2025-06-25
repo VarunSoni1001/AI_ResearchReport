5868ac1810e99d56608849f910dcb00d6ba0277d
Varun Soni <varun1001soni@gmail.com>
2025-06-25 12:00:22 +0530

    refactor: error states for ENV contants

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import './App.css'
-import { ENV } from './constants/env'
 
 function App() {
   return (
-    <div className='mx-auto min-w-4xl p-4 bg-amber-600'>
-      {ENV.APP_NAME}
-    </div>
+    <></>
   )
 }
 
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-  APP_NAME: import.meta.env.VITE_APP_NAME || "My App",
-  APP_VERSION: import.meta.env.VITE_APP_VERSION || "1.0.0",
-  API_URL: import.meta.env.VITE_APP_BASE_URL || "http://localhost:3000/api",
-  GOOGLE_GEMINI_API_KEY: import.meta.env.VITE_GOOGLE_GEMINI_API_KEY || "",
+  APP_NAME: import.meta.env.VITE_APP_NAME || "app_name_not_defined_in_env",
+  APP_VERSION: import.meta.env.VITE_APP_VERSION || "app_version_not_defined_in_env",
+  API_URL: import.meta.env.VITE_APP_BASE_URL || "app_base_url_not_defined_in_env",
+  GOOGLE_GEMINI_API_KEY: import.meta.env.VITE_GOOGLE_GEMINI_API_KEY || "api_key_not_defined_in_env",
 };
