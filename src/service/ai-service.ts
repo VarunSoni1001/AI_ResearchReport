50c30ceb5625158fc458f71bcd3a94816f3fd130
Varun Soni <varun1001soni@gmail.com>
2025-06-25 12:17:02 +0530

    feat: added google's genai(new) and generative-ai(old) packages, added basic service with both old and new methods for research reports

---
+++ src/service/ai-service.ts
@@ -0,0 +1,105 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { ENV } from "../constants/env";
+import { GoogleGenAI } from "@google/genai";
+
+const GEMINI_API_KEY = ENV.GOOGLE_GEMINI_API_KEY;
+
+if (!GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY is not defined");
+}
+
+// OLD
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const getReport = async (prompt: { keywords: string[] }) => {
+  let systemInstruction = "";
+
+  if (prompt.keywords.length > 0) {
+    systemInstruction = `You are an expert in generating research reports based on the following keywords: ${prompt.keywords.join(
+      ", "
+    )}. Please provide a detailed report.`;
+  } else {
+    systemInstruction =
+      "You are an expert in generating research reports. Please provide a detailed report.";
+  }
+
+  try {
+    const result = await model.generateContent({
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: prompt.keywords.join(", ") }],
+        },
+      ],
+      generationConfig: {
+        maxOutputTokens: 1000,
+        temperature: 0.5,
+        topP: 1,
+        frequencyPenalty: 0,
+        presencePenalty: 0,
+      },
+      systemInstruction,
+    });
+
+    const response = await result.response.text();
+    return response;
+  } catch (error) {
+    console.error("Error fetching RAG response:", error);
+    return "Error generating response";
+  }
+};
+
+// NEW
+const ai = new GoogleGenAI({
+  apiKey: GEMINI_API_KEY,
+});
+const aiModel = "gemini-1.5-flash";
+
+const getReportNew = async (prompt: { keywords: string[] }) => {
+  let systemInstruction = "";
+
+  if (prompt.keywords.length > 0) {
+    systemInstruction = `You are an expert in generating research reports based on the following keywords: ${prompt.keywords.join(
+      ", "
+    )}. Please provide a detailed report.`;
+  } else {
+    systemInstruction =
+      "You are an expert in generating research reports. Please provide a detailed report.";
+  }
+
+  try {
+    const result = await ai.models.generateContent({
+      model: aiModel,
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: prompt.keywords.join(", ") }],
+        },
+      ],
+      config: {
+        // maxOutputTokens: 1000,
+        // temperature: 0.5,
+        // topP: 1,
+        // frequencyPenalty: 0,
+        // presencePenalty: 0,
+
+        // thinking configurations
+        thinkingConfig: {
+          thinkingBudget: 1000,
+          includeThoughts: true,
+        },
+      },
+    });
+
+    return result.text;
+  } catch (error) {
+    console.error("Error fetching Report Response:", error);
+    return `Error fetching Report Response: ${error}`;
+  }
+};
+
+export const aiService = {
+  getReport,
+  getReportNew,
+};
